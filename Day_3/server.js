56895b719577a08f23fb2869f54e6ce974d2a4a9
Srimathi K <msri90898@gmail.com>
2026-04-17 20:01:05 +0530

    Updated code

--- Day_3/server.js
+++ Day_3/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); 
 const Student = require("./models/Student");
 
 const app = express();
+app.use(cors());              
 app.use(express.json());
 
 app.post("/add", async (req, res) => {
